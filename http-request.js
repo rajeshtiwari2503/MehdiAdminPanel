669b5f971e3b6cacd535504fbe6e7bf45b230676
rajeshtiwari2503 <rajeshtiwari2503@gmail.com>
2024-10-28 14:59:25 +0530

    jhjhjhjhjhjhhbhghg

--- http-request.js
+++ http-request.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-const baseUrl= "http://localhost:5000"
-//  const baseUrl="https://mehdiappbackend.onrender.com"
+// const baseUrl= "http://localhost:5000"
+ const baseUrl="https://mehdiappbackend.onrender.com"
 
 
 const get=(url)=>{
