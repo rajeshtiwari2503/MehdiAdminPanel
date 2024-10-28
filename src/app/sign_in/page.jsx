100b6f4c7912c1f18f64e342d846ffb321d0c9bb
rajeshtiwari2503 <rajeshtiwari2503@gmail.com>
2024-10-28 14:58:53 +0530

    jhjhjhjhjhjh

--- src/app/sign_in/page.jsx
+++ src/app/sign_in/page.jsx
@@ -116,16 +116,18 @@ export default function SignIn() {
                 <form className="space-y-6" onSubmit={handleSubmit(onSubmit)}>
                   <div>
                     <label htmlFor="email" className="block text-sm font-medium leading-6 text-gray-900">
-                      Email address
+                      Email  or Contact No.
                     </label>
                     <div className="mt-2">
                       <input
                         id="email"
                         name="email"
-                        type="email"
+                        type="text"
                         autoComplete="email"
                         required
-                        {...register('email', { required: 'Email is required', pattern: { value: /^\S+@\S+$/i, message: 'Invalid email address' } })}
+                        {...register('email', { required: 'Email is required'
+                          // , pattern: { value: /^\S+@\S+$/i, message: 'Invalid email address' }
+                         })}
                         className={`block p-3 w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 ${errors.email ? 'border-red-500' : ''}`}
                       />
                     </div>
